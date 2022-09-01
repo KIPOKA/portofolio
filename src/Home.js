f495a9e06dc97edf8d1717665e1e1a8fce7131bf
220825213 <220825213@tut4life.ac.za>
2022-09-02 01:18:04 +0200

    Working on the home and its css style

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import './Home.css';
-import logo from "./asset/logo.jpeg";
+import logo from "./img/logo.jpeg";
 
 
 function Home(){
     return(
-        <div className="home">
-
+        <div className="home"> 
                 <div className="home_bg">
-                    <div className="header d_flex align_items_center pxy_30">
+                    <div className="header d__flex align__items__center pxy__30">
                         <div className="logo">
                             <img src={logo} alt=""/> 
                         </div>
-                        <div className='navigation'>
-                            <ul className='navbar'>
-                                <a href='#Home'> <li className='nav_items mx_15'>Home</li></a>
-                                <a href='#Home'> <li className='nav_items mx_15'>Home</li></a>
-                                <a href='#Home'> <li className='nav_items mx_15'>Home</li></a>
-                                <a href='#Home'> <li className='nav_items mx_15'>Home</li></a>
-                                <a href='#Home'> <li className='nav_items mx_15'>Home</li></a>
+                        <div className="navigation pxy__30">
+                            <ul className="navbar d__flex">
+                                <a href="#Home"> <li className="nav__items mx__15">Home</li></a>
+                                <a href="#Services"> <li className="nav__items mx__15">Services</li></a>
+                                <a href="#Portofolio"> <li className="nav__items mx__15">Portofolio</li></a>
+                                <a href="#Blog"> <li className="nav__items mx__15">Blog</li></a>
+                                <a href="#About"> <li className="nav__items mx__15">About</li></a>
+                                <a href="#Contact"> <li className="nav__items mx__15">Contact </li></a>
 
                             </ul>
 
                         </div>
                     </div>
+                <div className="container">
+                        <div className="home__content">
+                            <div className="home__meta pz__10">
+                                <h1 className="home__text"> Welcome to my world of adventurer</h1>
+                            </div>
+
+                        
+                        </div>
+
+                </div>
                 </div>
 
         </div>

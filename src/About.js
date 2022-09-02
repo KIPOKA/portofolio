b31aac9f967fad9dc06a3bb8e692adc7b26e636b
220825213 <220825213@tut4life.ac.za>
2022-09-02 19:32:09 +0200

    Skills points fixed and leftt with writing the correct senttences

--- src/About.js
+++ src/About.js
@@ -10,6 +10,31 @@ function About() {
               <div className="col__2">
                   <img src={aboutImage} alt="" className="about__img"/>
               </div> 
+              <div className="col__2">
+                  <h1 className="about__heading">
+                      About Me!
+                  </h1>
+                  <div className="about__meta">
+                      <p className='about__text p__color'>
+                      The term CV is an abbreviation of the Latin word Curriculum Vitae, which is literally translated to “the course of your life”. 
+                      A CV is a very in-depth document that describes your career journey step-by-step, including all sorts of personal information. 
+                      You can look at the CV as a comprehensive description of everything you have ever done, all the achievements you are proud of,
+                    and all the publications that bear your name.     
+                      </p>
+                      <p className='about__text p__color'>
+                      The term CV is an abbreviation of the Latin word Curriculum Vitae, which is literally translated to “the course of your life”. 
+                      A CV is a very in-depth document that describes your career journey step-by-step, including all sorts of personal information. 
+                      You can look at the CV as a comprehensive description of everything you have ever done, all the achievements you are proud of,
+                    and all the publications that bear your name.     
+                      </p>
+                      <div className="about__button d__flex align__items__center">
+                          <a href="google.com"><button className="about btn pointer">Download CV</button></a>
+                          <a href="google.com"><button className="about btn pointer">Hire me </button></a>
+
+
+                      </div>
+                  </div>
+              </div>
           </div>
 
       </div>
--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-
-import About from './About';
-import './App.css';
-import Home from './Home';
+import "./App.css";
+import Home from "./Home";
+import About from "./About";
+import Service from "./Service";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <About/>
-
+      <Home />
+      <About />
+      <Service />
     </div>
   );
 }
